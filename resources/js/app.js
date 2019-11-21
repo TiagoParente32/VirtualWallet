d9781af1e420590f3b2ccad92c7b0eb4b53652aa
Tiago Parente <t.parente.32@gmail.com>
2019-11-21 14:13:14 +0000

    vue initial setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,29 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
+//import Users from './components/users'
+
+const routes = [
+    // {
+    //     path: '/',
+    //     component: Users
+    // }
+]
+const router = new VueRouter({
+    routes // == routes:routes
+})
+
+const app = new Vue({
+    el: '#app',
+    router,//==router:router
+    data: {
+
+    },
+    methods: {
+
+    }
+});
